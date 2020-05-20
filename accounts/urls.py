d561b0fe82bb32785b37c025a8c761a961964393
Jayesh Kumar <jayeshkr73608@gmail.com>
2020-05-20 17:15:39 +0530

    change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'signup/', views.SignUpView.as_view(template_name = 'accounts/signup.html'), name= 'signup_url'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name= 'login_url'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name= 'logout_url'),
+    url(r'^password_change/$', auth_views.PasswordChangeView.as_view(template_name = 'accounts/password_change.html'), name= 'password_change_url'),
+    url(r'^password_change/done/$', auth_views.PasswordChangeDoneView.as_view(template_name = 'accounts/password_change_done.html'), name= 'password_change_done_url'),
+
 ]
