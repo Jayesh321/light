7524a8b40121dfe9cc0304718e841aae1922c64f
Jayesh Kumar <jayeshkr73608@gmail.com>
2020-06-02 20:57:49 +0530

    signup changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,32 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core import validators
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    #phone_no = forms.CharField(max_length=50)
+    phone_no = forms.CharField(max_length=50)
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username', 'email','phone_no', 'first_name', 'last_name', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = 'Your User Name'
+    
+    def clean_email(self):
+        print('Total Form Validation by using single clean method...')
+        # total_cleaned_data=super().clean()
+        email = self.cleaned_data.get('email')
+        try:
+            match = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        
+        raise forms.ValidationError('This email address is already in use.')
+    
+        
+
--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 
 # Create your models here.
 class User(auth.models.User, auth.models.PermissionsMixin):
+    phone_no = models.CharField(max_length=50, default=True)
 
     def __str__(self):
         return self.username
